a694e2cd235aa90f5ba26cc0a019620c8bafb1c9
MarwahManan <marwahkhan671@gmail.com>
2024-12-21 20:32:10 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from 'react'
+import Hero from "./Components/Hero"
 import PopularCar from "./Components/PopularCar";
 import RecommendedCar from "./Components/RecommendedCars";
 
@@ -9,6 +10,7 @@ const Home = () => {
   return (
     <div> 
 
+      <Hero/>
       <PopularCar/>
       <RecommendedCar/>
     </div>
