a6cbb70336a78b74cb82f37e965ccd86392c61e7
MarwahManan <marwahkhan671@gmail.com>
2024-12-21 19:04:23 +0500

    Update page.tsx

--- src/app/billing/page.tsx
+++ src/app/billing/page.tsx
@@ -202,8 +202,8 @@ const  Billing = () => {
                 </div>
 
                 <div className='w-fit mt-4'>
-                   <Link href="/dashboard"><button className='bg-[rgba(53,99,233,100%)] text-white px-6 py-3 rounded-lg'>Rent Now</button>
-                </Link>
+                  <Link href="/">  <button className='bg-[rgba(53,99,233,100%)] text-white px-6 py-3 rounded-lg'>Rent Now</button>
+                  </Link>
                 </div>
 
                 <div className='mt-3'>
@@ -218,6 +218,7 @@ const  Billing = () => {
     </div>
     </>
   )
+                       
 }
 
 export default Billing;
