f98ad6c1b8940afe37495befc399b1dff538434d
MarwahManan <marwahkhan671@gmail.com>
2024-12-21 20:40:35 +0500

    Update cards1.tsx

--- src/app/Components/Categories/cards1.tsx
+++ src/app/Components/Categories/cards1.tsx
@@ -35,7 +35,7 @@ const PopularCars = () => {
       ];
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
       {cars.map((car) => (
         <div key={car.id} className="border rounded-lg p-6 shadow-md bg-white hover:shadow-lg transition-shadow">
           <div className="flex justify-between items-start">
@@ -44,11 +44,11 @@ const PopularCars = () => {
           </div>
           <p className="text-sm text-gray-500 mb-4">{car.type}</p>
           <Image src={car.image} alt={car.name} width={200} height={100} className="mx-auto object-contain" />
-          <div className="flex justify-between items-center mt-4">
+       
             <p className="text-lg font-semibold">{car.price}</p>
            <Link href="/billing"> <button className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600">Rent Now</button></Link>
           </div>
-        </div>
+       
       ))}
     </div>
   )
